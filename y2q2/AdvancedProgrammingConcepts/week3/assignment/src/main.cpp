6034cf10b54471c4aa99c008f2ec9426d19de130
SnakeXrnt <ethanbastian11@gmail.com>
2025-11-26 13:17:46 +0100

    working on APC

--- y2q2/AdvancedProgrammingConcepts/week3/assignment/src/main.cpp
+++ y2q2/AdvancedProgrammingConcepts/week3/assignment/src/main.cpp
@@ -1,20 +1,22 @@
 #include "logger.hpp"
 #include "program.hpp"
 #include "../writers/console_writer.hpp"
+#include "../writers/stream_writer.hpp"
 #include "../time/system_time_source.hpp"
 
+#include <algorithm>
 #include <memory>
 
 int main() {
     using writers::console_writer;
     using lib::logger;
+    using writers::stream_writer;
 
     // 1. Concrete writer
     auto writer = std::make_unique<console_writer>();
 
     // 2. Concrete logger, but stored as pointer to interface
-    std::unique_ptr<loggers::ilogger> log =
-        std::make_unique<logger>(std::move(writer));
+    std::unique_ptr<loggers::ilogger> log = std::make_unique<logger>(std::move(writer));
 
     // 3. Time source
     auto ts = std::make_unique<system_time_source>();
@@ -28,4 +30,17 @@ int main() {
     // 4. Transfer ownership of logger to program
     program prog{ std::move(log) };   // now program owns it
     prog.run();
+
+    std::cout << "file logging" << std::endl;
+
+    auto file_writter = std::make_unique<stream_writer>("log.txt");
+    std::unique_ptr<loggers::ilogger> file_log = std::make_unique<logger>(std::move(file_writter));
+    auto ts2 = std::make_unique<system_time_source>();
+    auto concrete_file_logger = static_cast<logger*>(file_log.get());
+    concrete_file_logger->set_time_source(std::move(ts2));
+    program prog2{ std::move(file_log) };   // now program owns it
+    prog2.run();
+
+
+
 }
--- y2q2/AdvancedProgrammingConcepts/week3/assignment/src/program.cpp
+++ y2q2/AdvancedProgrammingConcepts/week3/assignment/src/program.cpp
@@ -15,7 +15,7 @@ void program::run(){
     using namespace std::literals;
 
     auto n{1};
-    while(n <= 5){
+    while(n <= 500000){
         m_logger->log("Running: "s + std::to_string(n++));
     }
 }
